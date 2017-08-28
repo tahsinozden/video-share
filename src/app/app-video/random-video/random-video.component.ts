b8e3c5bd4afcd86c9403b6b063bc22c278ace6ac
tahsinozden <tahsinozden@gmail.com>
2017-08-29 00:56:35 +0200

    implemented autocomplete.

--- src/app/app-video/random-video/random-video.component.ts
+++ src/app/app-video/random-video/random-video.component.ts
@@ -1,68 +1,77 @@
-import { Observable } from 'rxjs';
-import { VideoService } from './../video.service';
-import { VideoTagModel } from './../video-tag.model';
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ViewChild } from '@angular/core';
-import { VideoModel } from './../video.model';
+import {Observable} from 'rxjs';
+import {VideoService} from './../video.service';
+import {VideoTagModel} from './../video-tag.model';
+import {Component, OnInit, Input, ElementRef} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ViewChild} from '@angular/core';
+import {VideoModel} from './../video.model';
 
 @Component({
-  selector: 'random-video',
-  templateUrl: './random-video.component.html',
-  styleUrls: ['./random-video.component.css'],
-  providers: []
+    selector: 'random-video',
+    templateUrl: './random-video.component.html',
+    styleUrls: ['./random-video.component.css'],
+    providers: []
 })
 export class RandomVideoComponent implements OnInit {
-  @Input() recentVideoClicked: VideoModel;
-  @ViewChild("videoplayer") videoplayer;
+    @Input() recentVideoClicked: VideoModel;
+    @ViewChild("videoplayer") videoplayer;
 
-  randomVideo: VideoModel;
-  currentVideoTags: Observable<VideoTagModel[]>;
+    randomVideo: VideoModel;
+    currentVideoTags: Observable<VideoTagModel[]>;
+    selectedVideoTagIds: string[] = [];
 
-  constructor(private http: HttpClient,
-              private videoService: VideoService) {
+    constructor(private http: HttpClient,
+                private videoService: VideoService) {
 
-    this.videoService.videoOnRecentBarClicked.subscribe(video => {
-      this.randomVideo = video;
-      this.currentVideoTags = this.videoService.getVideoTagsById(this.randomVideo.videoTagIds);
-      this.videoService.loadAndPlayVideo(this.videoplayer.nativeElement);
-    });                
-  }
-
-  loadRandVideo(video: HTMLVideoElement) {
-    if (this.randomVideo != undefined) {
-      this.videoService.newVideoAdded.emit(this.randomVideo);          
+        this.videoService.videoOnRecentBarClicked.subscribe(video => {
+            this.randomVideo = video;
+            this.currentVideoTags = this.videoService.getVideoTagsById(this.randomVideo.videoTagIds);
+            this.videoService.loadAndPlayVideo(this.videoplayer.nativeElement);
+        });
     }
-    
-    // this.videoService.loadRandomVideo()
-    //   .subscribe((data: VideoModel) => {
-    //     console.log(data);
-    //     data["url"] = this.videoService.BACKEND_URL + data["url"];
-    //     this.randomVideo = data;
-    //     this.videoService.loadAndPlayVideo(video);
-    // });
 
-    this.videoService.loadRandomVideoObject()
-      .subscribe(
-        serverVideo => {
-          this.randomVideo = new VideoModel(serverVideo.videoId, 
-                                            this.videoService.BACKEND_URL + serverVideo.videoFilePath, 
-                                            serverVideo.videoTagIds.split(","));
-          console.log(this.randomVideo);            
-          // this.videoService.getVideoTagsById(this.randomVideo.videoTagIds)
-          //   .subscribe((data: VideoTagModel[]) => {
-          //     console.log(data);
-          //   });                
-          this.currentVideoTags = this.videoService.getVideoTagsById(this.randomVideo.videoTagIds);                
-          this.videoService.loadAndPlayVideo(video);
-        },
-        error => {
-          console.log(error);
+    loadRandVideo(video: HTMLVideoElement) {
+        if (this.randomVideo != undefined) {
+            this.videoService.newVideoAdded.emit(this.randomVideo);
         }
-    )
-  }
 
-  ngOnInit() {
-  }
+        // this.videoService.loadRandomVideo()
+        //   .subscribe((data: VideoModel) => {
+        //     console.log(data);
+        //     data["url"] = this.videoService.BACKEND_URL + data["url"];
+        //     this.randomVideo = data;
+        //     this.videoService.loadAndPlayVideo(video);
+        // });
+
+        this.videoService.loadRandomVideoObjectWithTags(this.selectedVideoTagIds)
+            .subscribe(
+                serverVideo => {
+                    this.randomVideo = new VideoModel(serverVideo.videoId,
+                        this.videoService.BACKEND_URL + serverVideo.videoFilePath,
+                        serverVideo.videoTagIds.split(","));
+                    console.log(this.randomVideo);
+                    // this.videoService.getVideoTagsById(this.randomVideo.videoTagIds)
+                    //   .subscribe((data: VideoTagModel[]) => {
+                    //     console.log(data);
+                    //   });
+                    this.currentVideoTags = this.videoService.getVideoTagsById(this.randomVideo.videoTagIds);
+                    this.videoService.loadAndPlayVideo(video);
+                },
+                error => {
+                    console.log(error);
+                }
+            )
+    }
+
+    onSelectedVideoTags(videoTagIds: string[]) {
+        this.selectedVideoTagIds = videoTagIds;
+        console.log("selected!");
+    }
+
+    ngOnInit() {
+        this.videoService.videoTagSelectedForRandomVideo.subscribe((data: string[]) => {
+            this.selectedVideoTagIds = data;
+        })
+    }
 
 }
--- src/app/app-video/random-video/video-tag-selector/video-tag-selector.component.ts
+++ src/app/app-video/random-video/video-tag-selector/video-tag-selector.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
@@ -7,46 +7,69 @@ import {VideoService} from "../../video.service";
 import {VideoTagModel} from "../../video-tag.model";
 
 @Component({
-  selector: 'video-tag-selector',
-  templateUrl: './video-tag-selector.component.html',
-  styleUrls: ['./video-tag-selector.component.css']
+    selector: 'video-tag-selector',
+    templateUrl: './video-tag-selector.component.html',
+    styleUrls: ['./video-tag-selector.component.css']
 })
 export class VideoTagSelectorComponent implements OnInit {
-  public model: any;
-  allVideoTags: VideoTagModel[] = [];
-  selectedVideoTags: string[] = [];
+    model: string = "";
+    allVideoTags: VideoTagModel[] = [];
+    selectedVideoTags: VideoTagModel[] = [];
+    formatter = (result: VideoTagModel) => result.tagName;
+    // ViewChild must be used to reach the html reference
+    @ViewChild('searchElm') searchElm: ElementRef;
 
-  constructor(private videoService: VideoService) {
-  }
-
-  search = (text$: Observable<string>) =>
-    text$
-      .debounceTime(200)
-      .distinctUntilChanged()
-      .map(term => term.length < 2 ? []
-        : this.allVideoTags.filter(v => v.tagName.toLowerCase().indexOf(term.toLowerCase()) > -1)
-          .map(item => item.tagName)
-          .slice(0, 10));
-
-  onTagSelected(event) {
-    if (event.item != "") {
-      this.selectedVideoTags.push(event.item);
+    constructor(private videoService: VideoService) {
     }
-  }
 
-  onTagRemove(tagName: string) {
-    const idx = this.selectedVideoTags.indexOf(tagName);
-    if (idx> -1) {
-      this.selectedVideoTags.splice(idx, 1);
+    search = (text$: Observable<string>) =>
+        text$
+            .debounceTime(200)
+            .distinctUntilChanged()
+            .map(term => term.length < 2 ? []
+                : this.allVideoTags.filter(v => v.tagName.toLowerCase().indexOf(term.toLowerCase()) > -1) )
+                    // .map(item => item.tagName)
+                    // .slice(0, 10));
+
+    onTagSelected(event) {
+        if (event.item != null) {
+            this.selectedVideoTags.push(event.item);
+            this.videoService.videoTagSelectedForRandomVideo.emit(this.convertToIdList(this.selectedVideoTags));
+            console.log(event.item.tagName);
+            this.searchElm.nativeElement.value = "";
+            this.model = event.item.tagName;
+        }
     }
-  }
 
-  ngOnInit() {
-    this.videoService.getAvailableVideoTags().subscribe(data => {
-      data.map(val => {
-        this.allVideoTags.push(val);
-      })
-    })
-  }
+    onTagRemove(tag: VideoTagModel) {
+        const idx = this.selectedVideoTags.indexOf(tag);
+
+        if (idx > -1) {
+            this.selectedVideoTags.splice(idx, 1);
+            this.videoService.videoTagSelectedForRandomVideo.emit(this.convertToIdList(this.selectedVideoTags));
+        }
+    }
+
+    private convertToIdList(videoTag: VideoTagModel[]): string[] {
+        console.log(videoTag);
+        return videoTag.map(item => item.tagId.toString());
+    }
+
+    private getTagIdFRomTagName(tagName: string) {
+        if (this.selectedVideoTags.length) {
+            return this.selectedVideoTags
+                .filter(item => item.tagName === tagName)
+                .map(item => item.tagId.toString());
+        }
+        return "";
+    }
+
+    ngOnInit() {
+        this.videoService.getAvailableVideoTags().subscribe(data => {
+            data.map(val => {
+                this.allVideoTags.push(val);
+            })
+        })
+    }
 
 }
